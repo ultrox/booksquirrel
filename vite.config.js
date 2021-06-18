0a8c64510369d8ff67ff2d01964221536bf6bdba
Marko Vujanic <crashxx@gmail.com>
2021-06-18 18:23:34 +0200

    Setup Aliases for paths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import path from 'path';
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+  resolve: {
+    alias: {
+      '@app': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+    },
+  },
+  plugins: [reactRefresh()],
+});
