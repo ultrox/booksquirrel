acce6e6e703bf3d36b35a741bf5f3542a348ee3c
Marko Vujanic <crashxx@gmail.com>
2021-06-18 20:48:46 +0200

    Add code quality tooling

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import path from 'path';
-import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import path from "path";
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      '@app': path.resolve(__dirname, 'src'),
-      '@components': path.resolve(__dirname, 'src/components'),
+      "@app": path.resolve(__dirname, "src"),
+      "@components": path.resolve(__dirname, "src/components"),
     },
   },
   plugins: [reactRefresh()],
